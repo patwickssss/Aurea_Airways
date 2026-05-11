17021ebca8854b97a9e3be83a6b6cdc7d4817f1b
patwickssss <patwickssss@gmail.com>
2026-05-11 18:20:04 +0800

    header-draft

---
+++ classes/header.php
@@ -0,0 +1,13 @@
+<style></style>
+<nav>
+    <img id="logo" src="" alt="LOGO">
+    <div class="nav">
+        <a href="">Lorem Ipsum</a>
+        <a href="">Lorem Ipsum</a>
+        <a href="">Lorem Ipsum</a>
+        <a href="">Lorem Ipsum</a>
+        <a href="">Lorem Ipsum</a>
+    </div>
+    
+    <button>Login</button>
+</nav>
--- index.php
+++ index.php
@@ -1,9 +1,15 @@
+<!-- This is the header -->
+<?php include 'classes/header.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landing Page</title>
+    <style>
+        <?php include 'style.css'; ?>
+    </style>
 </head>
 <body>
     
