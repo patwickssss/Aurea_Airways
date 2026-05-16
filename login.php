6039fa556413f311e62c3b6ed25d3467b700369e
patwickssss <patwickssss@gmail.com>
2026-05-16 19:06:24 +0800

    May 16 modification

--- login.php
+++ login.php
@@ -1,11 +1,19 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+
+
+<!-- This is the header -->
+<?php include 'static/header.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head><
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
+    <title>Landing Page</title>
+    <style>
+        <?php include 'style.css'; ?>;
+    </style>
 </head>
-<body>
-    
-</body>
+
+<?php include 'static/footer.php'; ?>
 </html>
--- static/header.php
+++ static/header.php
@@ -2,12 +2,12 @@
 <nav>
     <img id="logo" src="img/LOGO.svg" alt="LOGO">
     <div class="nav">
-        <a href="">Home</a>
+        <a href="index.php">Home</a>
         <a href="">Flights</a>
         <a href="">Destinations</a>
         <a href="">Promos</a>
         <a href="">Contact</a>
-        <a href="">Login/Signup</a>
+        <a href="login.php">Login</a>
         
     </div>
     
